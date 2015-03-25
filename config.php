7c88440ea7164e2407875ed00ee7dfb18a3d4dc4
andykeqin <keqin.feng.12@ucl.ac.uk>
2015-03-25 14:47:29 +0000

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 return array(
-    'DB_HOST' => 'localhost',
+    'DB_HOST' => 'eu-cdbr-azure-west-a.cloudapp.net',
     'DB_PORT' => 3306,
     'DB_NAME' => 'group13',
-    'DB_USER' => 'root',
-    'DB_PWD' => 'root',
+    'DB_USER' => 'b9e937204e7143',
+    'DB_PWD' => '4f8a7b9b',
     'DB_CHARSET' => 'utf-8'
 );
